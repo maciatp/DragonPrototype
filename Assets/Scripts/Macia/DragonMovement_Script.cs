27b8453e29ebdccfe35216247d032a6d66d4f39d
MaciàTP <maciatp@gmail.com>
2024-10-16 17:11:32 +0200

    OLD Scripts recovered

---
+++ Assets/Scripts/Macia/DragonMovement_Script.cs
@@ -0,0 +1,183 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragonMovement_Script : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    
+    public Transform GetDragonTransform() { return gameObject.transform; }
+
+    [SerializeField] float rollRate = 10;
+    [SerializeField] float pitchRate = 10;
+    [SerializeField] float yawRate = 10;
+    [SerializeField] float acceleration = 5f;
+    [SerializeField] float deceleration = 5f;
+    [SerializeField] float idleDeceleration = 5f;
+    [SerializeField] float currentFlyingSpeed = 20;
+    [SerializeField] float minFlyingSpeed = 10f;
+    [SerializeField] float idleFlyingSpeed = 20;
+    [SerializeField] float maxFlyingSpeed = 50f;
+    [SerializeField] float calledFlyingSpeed = 30;
+     float flyOffFlySpeed;
+
+
+    [SerializeField] private float circleRadius = 50f;              // Radio del círculo
+    [SerializeField] private float flyHeight = 10f;                  // Altura a la que vuela el dragón en círculo
+    [SerializeField] private float circleSpeed = 10f;               // Velocidad de desplazamiento en círculo
+
+    [SerializeField] DragonStates dragonState;
+   public DragonStates GetDragonState
+    {
+        get { return dragonState; }
+        set { dragonState = value; }
+    }
+
+    public enum DragonStates
+    {
+        Free,
+        Called,
+        Mounted,
+        Dismounted
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PARA TESTING en la escena dragon
+        if(SceneManager.GetActiveScene().name != "DragonTest_Scene")
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //VUELO EN CÍRCULO
+        if(dragonState == DragonStates.Free)
+        {
+            FlyInCircle();
+        }
+
+        //LO LLAMO
+        if(dragonState == DragonStates.Called)
+        {
+            Vector3 moveVector = new Vector3(playerTransform.position.x - gameObject.transform.position.x, playerTransform.position.y - gameObject.transform.position.y, playerTransform.position.z - gameObject.transform.position.z);
+            gameObject.transform.position += moveVector * calledFlyingSpeed * Time.deltaTime;
+            
+        }
+
+        //VOLANDO
+        if(dragonState == DragonStates.Mounted)
+        {
+            // Control de aceleración
+            if (Input.GetButton("Fire1")) // Botón A para acelerar
+            {
+                currentFlyingSpeed += acceleration * Time.deltaTime; // Aumentar la velocidad
+                currentFlyingSpeed = Mathf.Clamp(currentFlyingSpeed, minFlyingSpeed, maxFlyingSpeed); // Limitar a la velocidad máxima
+            }
+            else if (Input.GetButton("Fire3")) // Botón X para desacelerar
+            {
+                currentFlyingSpeed -= deceleration * Time.deltaTime; // Disminuir la velocidad
+                currentFlyingSpeed = Mathf.Clamp(currentFlyingSpeed, minFlyingSpeed, maxFlyingSpeed); // Asegurar que no sea negativo
+            }
+            else
+            {
+                // Si no se presionan los botones, se aplica una desaceleración suave
+                currentFlyingSpeed = Mathf.MoveTowards(currentFlyingSpeed, idleFlyingSpeed, idleDeceleration * Time.deltaTime);
+            }
+
+            transform.position += transform.forward * Time.deltaTime * currentFlyingSpeed;
+            transform.Rotate(Input.GetAxis("Vertical")*pitchRate, 0.0f*yawRate, -Input.GetAxis("Horizontal")*rollRate); //TODO: implement YAW
+        }
+
+
+        //DESMONTO
+        if(dragonState == DragonStates.Dismounted)
+        {
+            flyOffFlySpeed = maxFlyingSpeed * 1.25f;
+            transform.position += transform.forward * flyOffFlySpeed * Time.deltaTime;
+        }        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((other.tag == "Player") && dragonState != DragonStates.Mounted)
+        {
+           MountDragon(other);
+        }
+    }
+
+    public void MountDragon(Collider other)
+    {
+        //ROTO EL DRAGON PARA QUE ESTÉ CENTRADO
+        gameObject.transform.rotation = Quaternion.identity;
+        //COLOCO AL JUGADOR EN LA SILLA del dragon
+        other.transform.position = gameObject.transform.GetChild(0).transform.position;
+        //seteo el dragon como padre para que siga el movimiento
+        other.transform.SetParent(gameObject.transform.GetChild(0).transform);
+
+       // other.GetComponent<ThirdPersonMovement_Script>().IsOnDragon(gameObject);
+
+
+        SetDragonState(DragonStates.Mounted);
+    }
+
+    public void DismountDragon(GameObject player)
+    {
+        player.transform.SetParent(null);
+
+        //Camera.main.GetComponent<CinemachineBrain>().enabled = true;
+
+        SetDragonState(DragonStates.Dismounted);
+
+        StartCoroutine("DragonCooldownAfterJumpingOff");
+    }
+
+    IEnumerator DragonCooldownAfterJumpingOff()
+    {
+        
+        gameObject.GetComponent<SphereCollider>().enabled = false;
+        yield return new WaitForSeconds(2);
+        gameObject.GetComponent<SphereCollider>().enabled = true;
+        SetDragonState(DragonStates.Free);
+    }
+
+    private void FlyInCircle()
+    {
+        // Calcular la posición circular alrededor del jugador
+        if (playerTransform)
+        {
+            float angle = Time.time * circleSpeed; // Ángulo de rotación
+            Vector3 offset = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * circleRadius; // Cálculo del desplazamiento en círculo
+            Vector3 targetPosition = playerTransform.position + new Vector3(offset.x, flyHeight, offset.z); // Nueva posición en círculo
+
+
+            // Mover el dragón a la posición objetivo
+            transform.position = targetPosition;
+
+
+            // Hacer que el dragón mire hacia el interior del círculo
+            Vector3 tangentDirection = new Vector3(-Mathf.Sin(angle),0, Mathf.Cos(angle)); // Dirección tangente
+            Quaternion lookRotation = Quaternion.LookRotation(tangentDirection); // Crear la rotación que mira hacia el jugador
+            lookRotation *= Quaternion.Euler(-5, -15, 20); // Añadir rotación al dragón
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * yawRate); // Suavizar la rotación
+        }
+
+    }
+
+    public void SetDragonState(DragonStates _dragonState)
+    {
+        dragonState = _dragonState; // Método para cambiar el estado del dragón
+    }
+
+    public void CallDragon()
+    {
+        dragonState = DragonStates.Called;
+    }
+}
---
+++ Assets/Scripts/Macia/ThirdPersonMovement_Script.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonMovement_Script : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    Rigidbody rb;
+    
+  
+    [SerializeField] float speed = 6;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+
+    float velocityY;
+
+    bool isGrounded;
+    [SerializeField] float jumpForce = 3f;
+    float jumpHeight = 7f;
+    [SerializeField] LayerMask groundMask;
+   
+    [SerializeField] float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    [SerializeField] Transform cam;
+    //[SerializeField] Transform dragonSpawner;
+    //[SerializeField] GameObject dragon;
+    //[SerializeField] DragonMovement_Script dragonMovement_Script_;
+    //ThirdPersonCameraController tpCameraController;
+    //DragonCameraController dCameraController;
+    Vector2 moveInput;
+    [SerializeField] PlayerStates playerState = PlayerStates.Normal;
+
+    public enum PlayerStates
+    {
+        Normal,
+        BigFall,
+        OnDragon,
+    }
+    public PlayerStates GetPlayerState
+    {
+        get { return playerState; }
+    }
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnJump()
+    {
+        if(isGrounded )
+        {
+            
+            Jump();
+        }
+
+
+    }
+    void OnMove(InputValue movementValue)
+    {
+        moveInput = movementValue.Get<Vector2>();
+        
+    }
+
+
+    private void Update()
+    {
+        //JUMP
+        isGrounded = Physics.CheckSphere(groundCheck.position,groundDistance, groundMask);
+
+        if (isGrounded && playerState == PlayerStates.Normal)
+        {
+            Debug.Log("IS GROUNDED");
+            velocityY = 0;
+        }
+
+        if (playerState != PlayerStates.OnDragon && !isGrounded)
+        {
+
+            velocityY += Physics.gravity.y * Time.deltaTime;
+
+            controller.Move(new Vector3(0, velocityY * Time.deltaTime, 0));
+           
+        }
+
+
+        if (playerState == PlayerStates.Normal)
+        {
+            
+            //MOVEMENT
+            Vector3 direction = new Vector3(moveInput.x, velocityY, moveInput.y); // NORMALIZED para que no vaya más rápido cuando te mueves en diagonal
+
+
+            if (direction.magnitude >= 0.1f)
+            {
+
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //le sumo la rotación de la cámara para que camine a donde apunta la cam
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //para que gire suavemente
+
+                if (playerState != PlayerStates.BigFall)
+                {
+                    transform.rotation = Quaternion.Euler(0, angle, 0f);
+                }
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward + new Vector3(0,velocityY,0);
+
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
+            
+
+            if (!isGrounded && (velocityY <= -15f) && playerState != PlayerStates.BigFall && playerState != PlayerStates.OnDragon)
+            {
+                SetPlayerState(PlayerStates.BigFall);
+                Debug.Log("CAIDA!");
+                // float angle = Mathf.SmoothDampAngle(transform.eulerAngles.x, 90, ref turnSmoothVelocity, turnSmoothTime);
+                transform.GetChild(0).rotation = Quaternion.Euler(90, gameObject.transform.eulerAngles.y, gameObject.transform.eulerAngles.z);
+            }
+
+        }
+
+        if (isGrounded && playerState == PlayerStates.BigFall)
+        {
+            RepositionAfterBigFall();
+            Debug.Log("Reposiciono después de caída");
+        }
+    }
+
+    //private void Update()
+    //{
+
+
+    //    
+
+    //    if(playerState != PlayerStates.OnDragon)
+    //    {
+
+    //        if (isGrounded && velocity.y < 0)
+    //        {
+    //           
+
+    //            if (playerState == PlayerStates.BigFall)
+    //            {
+    //                //RECOLOCACION DESPUES DE CAIDA
+    //                RepositionAfterBigFall();
+    //            }
+    //        }
+
+    //       
+
+    //        
+    //        
+
+
+
+    //       
+
+    //        
+
+
+
+    //        //CALL DRAGON
+    //        if (playerState == PlayerStates.BigFall && Input.GetButtonDown("Jump"))
+    //        {
+    //            //CALL DRAGON
+    //            dragon.transform.position = dragonSpawner.position;
+    //            dragon.transform.rotation = dragonSpawner.rotation;
+    //            dragonMovement_Script_.CallDragon();
+
+    //        }
+
+    //    }
+
+    //    //ON DRAGON
+
+    //    if(playerState == PlayerStates.OnDragon && Input.GetButtonDown("Jump"))
+    //    {
+    //        Debug.Log("pulso botón para desmontar");
+    //        Jump();
+    //        IsNOTonDragon();
+    //    }
+    //}
+
+    private void Jump()
+    {
+        Debug.Log("JUMP");
+        //controller.SimpleMove(new Vector3(0, Mathf.Sqrt(jumpHeight * -Physics.gravity.y),0));
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        //velocityY += Mathf.Sqrt(jumpHeight * -Physics.gravity.y);
+    }
+
+    private void RepositionAfterBigFall()
+    {
+        transform.GetChild(0).rotation = Quaternion.Euler(0, gameObject.transform.rotation.eulerAngles.y, 0);
+        SetPlayerState(PlayerStates.Normal);
+    }
+
+    //public void IsOnDragon(GameObject dragon_)
+    //{
+    //    RepositionAfterBigFall();
+
+    //    SetPlayerState(PlayerStates.OnDragon);
+    //    velocity.y = 0;
+
+
+    //    //CAMBIO CÁMARA
+    //    Camera.main.GetComponent<DragonCameraController>().enabled = true;
+    //    Camera.main.GetComponent<ThirdPersonCameraController>().enabled = false;
+
+
+    //    gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    //}
+    //public void IsNOTonDragon()
+    //{
+    //    Debug.Log("is not on Dragon");
+    //    SetPlayerState(PlayerStates.Normal);
+    //    dragonMovement_Script_.DismountDragon(gameObject);
+    //    //CAMBIO CÁMARA
+    //    Camera.main.GetComponent<DragonCameraController>().enabled = false;
+    //    Camera.main.GetComponent<ThirdPersonCameraController>().enabled = true;
+    //}
+
+    void SetPlayerState(PlayerStates _state)
+    {
+        playerState = _state;
+    }
+}
