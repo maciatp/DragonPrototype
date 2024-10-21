a37ff2e7f8268e883989bb9289c39d75893581bf
MaciàTP <maciatp@gmail.com>
2024-10-21 15:03:19 +0200

    LandDragon Mounted Code Implementation

--- Assets/Scripts/Macia/DragonController.cs
+++ Assets/Scripts/Macia/DragonController.cs
@@ -44,6 +44,19 @@ public class DragonController : MonoBehaviour
     //DISMOUNT
     bool isFlyAwayCoroutineCalled = false;
 
+    //LANDING MOUNTED
+    private bool canLand = false; // Define si es posible aterrizar
+    Vector3 landingPoint;
+    [SerializeField] float landingForwardOffset = 10;
+    [SerializeField] float landMountedDuration = 2f; // Duración del aterrizaje cuando montas en el dragón
+    [SerializeField] float landingSpeedThreshold = 40f; //mínima velocidad para poder aterrizar
+    [SerializeField] float distanceToLand = 10f;
+    //LANDING MOUNTED DEBUG
+    [SerializeField] GameObject landingDebug;
+
+    //LANDING UNMOUNTEd
+    [SerializeField] float landUnmountedDuration = 2f; // Duración del aterrizaje cuando NO MONTAS
+
     //LANDED
     [SerializeField] BoxCollider landedCollider;
     [SerializeField] private LayerMask groundMask;
@@ -140,12 +153,23 @@ public class DragonController : MonoBehaviour
         }
     }
 
+    //LAND WHILE MOUNTED
+    public void OnLandDragon(InputAction.CallbackContext landDragonContext)
+    {
+        if (landDragonContext.performed && dragonState == DragonStates.Mounted)
+        {
+            Debug.Log("Acción de LAND Dragon");
+            PrepareLandingMounted();
+        }
+    }
+
     //LANDED MOVEMENT
     public void OnMoveLanded(InputAction.CallbackContext moveLandedContext)
     {
         moveInput = moveLandedContext.ReadValue<Vector2>();
     }
-
+   
+    //TAKE OFF
     public void OnTakeOff(InputAction.CallbackContext takeOffContext)
     {
         if(takeOffContext.performed && (dragonState == DragonStates.Landed || dragonState == DragonStates.MountedLanded))
@@ -171,7 +195,44 @@ public class DragonController : MonoBehaviour
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
         
-        lastPosition = transform.position;      
+        lastPosition = transform.position;
+
+        if (dragonState == DragonStates.Mounted && currentDragonSpeed < landingSpeedThreshold)
+        {
+            // Selecciona un punto en el suelo delante del dragón.
+            Vector3 forwardDirection = transform.forward;
+            landingPoint = playerTransform.position + forwardDirection * landingForwardOffset; // landingPoint es landingForwardOffset unidades adelante
+
+            // Hacer raycast hacia abajo desde el punto calculado para encontrar el suelo.
+            RaycastHit hit;
+            if (Physics.Raycast(landingPoint, Vector3.down, out hit, distanceToLand, groundMask))
+            {
+                // Si el raycast encuentra el suelo, ajusta la altura del punto de aterrizaje.
+                landingPoint.y = hit.point.y - dragonObj.transform.GetChild(0).transform.localPosition.y;
+                landingDebug.transform.position = landingPoint;
+                // Ahora puedes aterrizar
+                canLand = true;
+                Debug.Log("canLand es " + canLand + ", y landingPoint es " + landingPoint);
+            }
+            else
+            {
+                canLand = false;
+                //landingPoint = Vector3.zero; //probar con esta línea si algo da problemas en el aterrizaje
+                Debug.Log("canLand es " + canLand);
+                //DEBUG
+                landingDebug.transform.localPosition = Vector3.zero;
+
+            }
+        }
+        else
+        {            
+            canLand = false;
+            //landingPoint = Vector3.zero; //probar con esta línea si algo da problemas en el aterrizaje
+            Debug.Log("canLand es " + canLand);
+            //DEBUG
+            landingDebug.transform.localPosition = Vector3.zero;
+        }
+       
     }
     private void FixedUpdate()
     {
@@ -355,27 +416,44 @@ public class DragonController : MonoBehaviour
             
         }
     }
+    //LANDING UNMOUNTED
     public void CallDragonToLand()
     {
 
-        // Selecciona un punto aleatorio en un cono frente al player
+        // Selecciona un punto aleatorio en un cono frente a la cámara
         Vector3 randomPointInView = GetRandomPointInCone(playerTransform);
         
         SetDragonState(DragonStates.Landing);
         calledCollider.enabled = false;
-        StartCoroutine(LandAtPoint(randomPointInView));
+        StartCoroutine(LandAtPointUnmounted(randomPointInView));
+    }
+    private Vector3 GetRandomPointInCone(Transform player)
+    {
+        float angle = 45f; // Ángulo del cono
+        float distance = 10f; // Distancia máxima del aterrizaje
+        Vector3 forward = Camera.main.transform.forward;
+
+        Vector3 randomDirection = Quaternion.Euler(
+            Random.Range(-angle / 2, angle / 2),
+            Random.Range(-angle / 2, angle / 2),
+            0) * forward;
+
+        Vector3 randomPoint = player.position + randomDirection * Random.Range(5f, distance);
+        //AÑADIR COMPROBACIÓN DE HIT CON RAYCAST (para que se ponga a la altura del suelo)
+        randomPoint.y = player.position.y; // Mantén la altura similar al jugador
+
+        return randomPoint;
     }
 
-    private IEnumerator LandAtPoint(Vector3 targetPosition)
+    private IEnumerator LandAtPointUnmounted(Vector3 targetPosition)
     {
         RotateTowardsMovement(targetPosition);
-        float landDuration = 2f; // Duración del aterrizaje
         float elapsedTime = 0f;
 
         Vector3 startPosition = transform.position;
-        while (elapsedTime < landDuration)
+        while (elapsedTime < landUnmountedDuration)
         {
-            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / landDuration);
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / landUnmountedDuration);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
@@ -386,22 +464,38 @@ public class DragonController : MonoBehaviour
         SetDragonState(DragonStates.Landed); // O cualquier otro estado
         landedCollider.enabled = true;
     }
-
-    private Vector3 GetRandomPointInCone(Transform player)
+    //LANDING MOUNTED
+    private void PrepareLandingMounted()
     {
-        float angle = 45f; // Ángulo del cono
-        float distance = 10f; // Distancia máxima del aterrizaje
-        Vector3 forward = player.forward;
+        if (canLand)
+        {
+            StartCoroutine(LandAtPointWhenMounted(landingPoint));
+        }
+    }
+    private IEnumerator LandAtPointWhenMounted(Vector3 targetPosition)
+    {
+        SetDragonState(DragonStates.Landing);
 
-        Vector3 randomDirection = Quaternion.Euler(
-            Random.Range(-angle / 2, angle / 2),
-            Random.Range(-angle / 2, angle / 2),
-            0) * forward;
+        RotateTowardsMovement(targetPosition);
 
-        Vector3 randomPoint = player.position + randomDirection * Random.Range(5f, distance);
-        randomPoint.y = player.position.y; // Mantén la altura similar al jugador
+        float elapsedTime = 0f;
 
-        return randomPoint;
+        Vector3 startPosition = transform.position;
+        while (elapsedTime < landMountedDuration)
+        {
+            transform.position = Vector3.Slerp(startPosition, targetPosition, elapsedTime / landMountedDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+        SetDragonState(DragonStates.MountedLanded);
+        //Igualo rotación del gameobject padre al interno        
+        dragonObj.localRotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        landedCollider.enabled = true;
+        calledCollider.enabled = false;
+        SetDragonCamera();
     }
 
     //MOUNT
